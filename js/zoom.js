a4376c171c0af4dd76789329e306c44c27d0cb20
son426 <coghks0426@naver.com>
2022-10-08 22:31:48 +0900

    video

--- js/zoom.js
+++ js/zoom.js
@@ -1,19 +1,28 @@
-let contentImgArr = document.querySelectorAll(".content-img");
+let contentImgArr = document.querySelectorAll(".content-img iframe");
+
+const PLAYKEY = "?autoplay=1&mute=1";
+const PAUSEKEY = "?autoplay=0&mute=0";
 
 function zoomIn(e) {
   e.target.parentElement.parentElement.style.transform = "scale(1.1)";
   e.target.parentElement.parentElement.style.border = "1px black solid";
   e.target.parentElement.parentElement.style.zIndex = 1;
-  console.log("!");
+
+  if (e.target.src.includes(PAUSEKEY)) {
+    e.target.src = e.target.src.replace(PAUSEKEY, PLAYKEY);
+  } else {
+    e.target.src += PLAYKEY;
+  }
 }
 
 function zoomOut(e) {
   e.target.parentElement.parentElement.style.transform = "scale(1)";
   e.target.parentElement.parentElement.style.border = "none";
   e.target.parentElement.parentElement.style.zIndex = "auto";
+  e.target.src = e.target.src.replace(PLAYKEY, PAUSEKEY);
 }
 
 for (let i = 0; i < contentArr.length; i++) {
-  contentImgArr[i].addEventListener("mouseover", zoomIn);
-  contentImgArr[i].addEventListener("mouseout", zoomOut);
+  contentImgArr[i].addEventListener("mouseenter", zoomIn);
+  contentImgArr[i].addEventListener("mouseleave", zoomOut);
 }
