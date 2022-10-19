931b42a2b2b95c99ee38354d38b79b2d4de2639c
son426 <coghks0426@naver.com>
2022-10-19 19:31:48 +0900

    Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,31 +1,46 @@
-import userEvent from "@testing-library/user-event";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState([]);
-
-  const onSubmit = (event) => {
-    event.preventDefault();
-    setTodos((currentArray) => [todo, ...currentArray]);
-    event.target[0].value = "";
-  };
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [dollar, setDollar] = useState(0);
 
   const onChange = (event) => {
-    setTodo(event.target.value);
+    setDollar(event.target.value);
   };
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <div>
-      <h3>To Do List ({todos.length})</h3>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} type="text" placeholder="입력하세요" />
-        <button>저장</button>
-      </form>
-      <hr />
-      {todos.map((element, index) => (
-        <li key={index}>{element}</li>
-      ))}
+      <h1>The coins {loading ? null : `(${coins.length})`}</h1>
+      {loading ? (
+        <h3>Loading..</h3>
+      ) : (
+        <select>
+          {loading
+            ? null
+            : coins.map((element) => (
+                <option key={element.id}>
+                  {element.name} // {element.symbol} : $
+                  {element.quotes.USD.price}
+                  // {dollar / parseFloat(element.quotes.USD.price)}개 살 수
+                  있음.
+                </option>
+              ))}
+        </select>
+      )}
+      <div>
+        <label htmlFor="dollarInput">몇달러</label>
+        <input onChange={onChange} id="dollarInput" type="number" />
+      </div>
     </div>
   );
 }
