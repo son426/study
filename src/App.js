2ad464ac772d84c8125322fabf01926c431dda5c
son426 <coghks0426@naver.com>
2022-10-19 16:44:04 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,127 +1,58 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { useState } from "react";
-import { eventWrapper } from "@testing-library/user-event/dist/utils";
-
-function Header(props) {
-  return (
-    <header>
-      <a
-        href="/"
-        onClick={(event) => {
-          event.preventDefault();
-          props.onChangeMode();
-        }}
-      >
-        {props.title}
-      </a>
-    </header>
-  );
-}
-
-function Nav(props) {
-  const lis = [];
-  for (let i = 0; i < props.topics.length; i++) {
-    let t = props.topics[i];
-    lis.push(
-      <li key={t.id}>
-        <a
-          id={t.id}
-          href={"/read/" + t.id}
-          onClick={(event) => {
-            event.preventDefault();
-            props.onChangeMode(event.target.id);
-          }}
-        >
-          {t.title}
-        </a>
-      </li>
-    );
-  }
-
-  return <ol>{lis}</ol>;
-}
-function Article(props) {
-  return (
-    <article>
-      <h2>{props.title}</h2>
-      {props.body}
-    </article>
-  );
-}
-
-function Create() {
-  return (
-    <article>
-      <h2>Create</h2>
-      <form>
-        <p>
-          <input type="text" name="title" placeholder="제목 입력"></input>
-        </p>
-        <p>
-          <textarea name="body" placeholder="본문 입력"></textarea>
-        </p>
-        <p>
-          <input type="submit" value="Create"></input>
-        </p>
-      </form>
-    </article>
-  );
-}
+import { useState, useEffect } from "react";
 
 function App() {
-  // const _mode = useState("WELCOME");
-  // const mode = _mode[0];
-  // const setMode = _mode[1];
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => {
+    setCounter((prev) => prev + 1);
+  };
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+  useEffect(() => {
+    console.log("i run once");
+  }, []);
 
-  const [mode, setMode] = useState("CREATE");
-  const [id, setId] = useState(null);
-  const topics = [
-    { id: 1, title: "html", body: "html is ~" },
-    { id: 2, title: "css", body: "css is ~" },
-    { id: 3, title: "js", body: "js is ~" },
-  ];
+  useEffect(() => {
+    console.log("키워드변화");
+  }, [keyword]);
 
-  let content = null;
-  if (mode === "WELCOME") {
-    content = <Article title="Welcome" body="Hello, WEB"></Article>;
-  } else if (mode === "READ") {
-    let title = null;
-    let body = null;
-    for (let i = 0; i < topics.length; i++) {
-      if (topics[i].id === Number(id)) {
-        title = topics[i].title;
-        body = topics[i].body;
-      }
-    }
-    content = <Article title={title} body={body}></Article>;
-  } else if (mode === "CREATE") {
-    content = <Create></Create>;
-  }
+  useEffect(() => {
+    console.log("카운터변화");
+  }, [counter]);
+
+  useEffect(() => {
+    console.log("둘 중 하나라도 변화");
+  }, [keyword, counter]);
+
+  // cleanup func
+
+  const [showing, setShowing] = useState(false);
+  const cleanUp = () => {
+    setShowing((prev) => !prev);
+  };
+
+  const Hello = () => {
+    useEffect(() => {
+      console.log("hi");
+      return () => console.log("bye");
+    }, []);
+
+    return <h1>Hello</h1>;
+  };
 
   return (
     <div>
-      <Header
-        title="WEB"
-        onChangeMode={() => {
-          setMode("WELCOME");
-        }}
-      ></Header>
-      <Nav
-        topics={topics}
-        onChangeMode={(id) => {
-          setMode("READ");
-          setId(id);
-        }}
-      ></Nav>
-      {content}
-      <a
-        href="/create"
-        onClick={(event) => {
-          event.preventDefault();
-          setMode("CREATE");
-        }}
-      ></a>
+      <div>
+        <input onChange={onChange} type="text" placeholder="검색창" />
+        <h1>{counter}</h1>
+        <button onClick={onClick}>Click me!!</button>
+      </div>
+      <hr />
+      <div>
+        {showing ? <Hello /> : null}
+        <button onClick={cleanUp}>{showing ? "hide" : "show"}</button>
+      </div>
     </div>
   );
 }
