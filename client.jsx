c444683e7dc4319fad938ea77fc538e898ee2b3d
son426 <coghks0426@naver.com>
2022-11-03 20:36:37 +0900

    반응속도 테스트 / 클래스 & 훅스
    useRef / new Date() / setTimeout

--- client.jsx
+++ client.jsx
@@ -2,7 +2,9 @@ const React = require("react");
 const ReactDOM = require("react-dom");
 
 const WordRelay = require("./끝말잇기/WordRelay");
-const NumberBaseball_class = require("./NumberBaseball-class");
-const NumberBaseball_hooks = require("./NumberBaseball-hooks");
+const NumberBaseball_class = require("./숫자야구/NumberBaseball-class");
+const NumberBaseball_hooks = require("./숫자야구/NumberBaseball-hooks");
+const ResponseCheck_class = require("./ResponseCheck-class");
+const ResponseCheck_hooks = require("./ResponseCheck-hooks");
 
-ReactDOM.render(<NumberBaseball_hooks />, document.querySelector("#root"));
+ReactDOM.render(<ResponseCheck_hooks />, document.querySelector("#root"));
