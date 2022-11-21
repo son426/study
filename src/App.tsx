5438d86425afcc21c7607b0d37ebbc6674471576
son426 <coghks0426@naver.com>
2022-11-21 17:14:26 +0900

    CRYPTO TRACKER_0 / setting & 화면1(Coins)

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,79 @@
-import { useState } from "react";
-import styled, { keyframes } from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import Router from "./Router";
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+font-family: 'Source Sans Pro', sans-serif;
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, menu, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+main, menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure,
+footer, header, hgroup, main, menu, nav, section {
+  display: block;
+}
+/* HTML5 hidden-attribute fix for newer browsers */
+*[hidden] {
+    display: none;
+}
+body {
+  line-height: 1;
+}
+menu, ol, ul {
+  list-style: none;
+}
+blockquote, q {
+  quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+  content: '';
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+*{
+ box-sizing:border-box; 
+}
+body{
+  font-family : 'Source Sans Pro', sans-serif;
+  background-color:${(props) => props.theme.bgColor};
+  color:${(props) => props.theme.textColor};
+
+}
+a{
+  display:block;
+  text-decoration:none;
+  color:inherit;
+}
+`;
 
 function App() {
-  const [username, setUsername] = useState("");
-
-  const onChangeInput = (event: React.FormEvent<HTMLInputElement>) => {
-    setUsername(event.currentTarget.value);
-  };
-
-  const onSubmitForm = (event: React.SyntheticEvent) => {
-    event.preventDefault();
-    console.log(username);
-    setUsername("");
-  };
-
   return (
-    <div>
-      <form onSubmit={onSubmitForm}>
-        <input
-          onChange={onChangeInput}
-          value={username}
-          type="text"
-          placeholder="username"
-        />
-        <button>제출</button>
-      </form>
-    </div>
+    <>
+      <GlobalStyle></GlobalStyle>
+      <Router />
+    </>
   );
 }
 
