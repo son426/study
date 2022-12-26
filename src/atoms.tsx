a05285d96139bf0418bbda65f3c2235b533ddbb0
son426 <coghks0426@naver.com>
2022-12-26 14:21:37 +0900

    selector get/set

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,31 +1,18 @@
 import { atom, selector } from "recoil";
 
-export interface ITodo {
-  text: string;
-  id: number;
-  category: "TO_DO" | "DOING" | "DONE";
-}
-
-export const todoState = atom<ITodo[]>({
-  key: "toDo",
-  default: [],
+export const minuteState = atom({
+  key: "minute",
+  default: 0,
 });
 
-// export const toDoState = atom<IToDo[]>({
-//   key: "toDo",
-//   default: [],
-// });
-
-// export const categoryState = atom<"TO_DO" | "DOING" | "DONE">({
-//   key: "category",
-//   default: "TO_DO",
-// });
-
-// export const toDoSelector = selector({
-//   key: "toDoSelector",
-//   get: ({ get }) => {
-//     const toDos = get(toDoState);
-//     const category = get(categoryState);
-//     return toDos.filter((toDo) => toDo.category === category);
-//   },
-// });
+export const hourSelector = selector({
+  key: "hours",
+  get: ({ get }) => {
+    const minutes = get(minuteState);
+    return minutes / 60;
+  },
+  set: ({ set }, newValue) => {
+    const minutes = Number(newValue) * 60;
+    set(minuteState, minutes);
+  },
+});
